2d4348b428852e40b6f225b548650845ffc4af7c
shrubom <shruti.panicker21@gmail.com>
2022-09-19 14:06:08 +0300

    commit

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -5,7 +5,26 @@ describe("My first test suite", function () {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get("input.search-keyword").type("ca");
     //To validate how many objects are displayed and assert it using chai framework
-    cy.wait(4000);
-    cy.get(".product:visible").should("have.length", 4);
+    cy.wait(2000);
+    // cy.get(".product:visible").should("have.length", 4);
+    //Parent child chaining.
+    cy.get(".products").find(".product").should("have.length", 4);
+    //Now clicking on add to cart button on the second product
+    cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
+    //How to dynamically click on products based on their name without providing the index of the item]
+    //Now the requirement is when it is capsicum only then add to cart
+    cy.get(".products")
+      .find(".product")
+      .each(($element1, index, $list) => {
+        //In here you are just grabbing the text of the product in the element list
+        //you need to use wrap when you are trying to click on a button that is a collection/list
+        let vegRequired = $element1.find("h4.product-name").text(); //Grabs the text on that element
+        if (vegRequired.includes("Cashews")) {
+          cy.wrap($element1)
+            .find("button[type=button]")
+            .contains("ADD TO CART")
+            .click();
+        }
+      });
   });
 });
