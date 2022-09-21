a1726ac643d94b746152d0d71cee1c94d26724de
shrubom <shruti.panicker21@gmail.com>
2022-09-21 15:40:52 +0300

    More on UI automation

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -8,7 +8,9 @@ describe("My first test suite", function () {
     cy.wait(2000);
     // cy.get(".product:visible").should("have.length", 4);
     //Parent child chaining.
-    cy.get(".products").find(".product").should("have.length", 4);
+    //Instead of using the element multiple times, We use this technique (aliasing)
+    cy.get(".products").as("productLocator");
+    cy.get("@productLocator").find(".product").should("have.length", 4);
     //Now clicking on add to cart button on the second product
     cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
     //How to dynamically click on products based on their name without providing the index of the item]
@@ -29,8 +31,26 @@ describe("My first test suite", function () {
     // let logo = cy.get("div.brand.greenlogo");
     // cy.log(logo.text());
     //Line 29 and 30 will not work.
+    //Assertions
+    cy.get("div.brand.greenlogo").should("have.text", "GREENKART");
     cy.get("div.brand.greenlogo").then(function (logoElement) {
       cy.log(logoElement.text());
     });
   });
+
+  it("My second test case", function () {
+    cy.log("clicking on the cart button and hitting proceed button");
+    cy.get(".cart-icon > img").click();
+    cy.get(
+      "div.container div.container div.cart div.cart-preview.active:nth-child(6) div.action-block:nth-child(2) > button:nth-child(1)"
+    ).click();
+    cy.get(".promoCode").type("1AK23");
+    cy.get(".promoBtn").click();
+    cy.wait(2000);
+    cy.get(
+      "div.container div.products-wrapper:nth-child(2) div.products div:nth-child(4) > button:nth-child(14)"
+    ).click();
+  });
+
+  it("My third test case", function () {});
 });
