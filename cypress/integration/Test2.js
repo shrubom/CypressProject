63768ab45eb4de166be8c5d679dc236f1c5dc9c8
shrubom <shruti.panicker21@gmail.com>
2022-09-23 04:15:51 +0300

    UI automation techniques

--- cypress/integration/Test2.js
+++ cypress/integration/Test2.js
@@ -3,6 +3,28 @@ describe("My second test suite", function () {
   it("My third test case", function () {
     cy.log("More UI automation Techniques");
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-    cy.get("#checkBoxOption1").check();
+    //Clicking on the checkbox and validating the correct checkbox is clicked.
+    cy.get("#checkBoxOption1")
+      .check()
+      .should("be.checked")
+      .and("have.value", "option1");
+    cy.get("#checkBoxOption1").uncheck().should("not.be.checked");
+    //here we are checking all the three checkboxes.
+    cy.get('input[type="checkbox"]').check(["option2", "option3"]);
   });
+  it("Dropdown validatioons", function () {
+    //Static dropdown
+    cy.get("#dropdown-class-example")
+      .select("option1")
+      .should("have.value", "option1");
+    //Dynamic Dropdowns
+    cy.get("#autocomplete").type("ind");
+    cy.get(".ui-menu-item div").each(($dynamicMenuItem, index, $list) => {
+      if ($dynamicMenuItem.text() === "India") {
+        cy.wrap($dynamicMenuItem).click();
+      }
+    });
+    cy.get("#autocomplete").should("have.value", "India");
+  });
+  it("Visibilty and invisibility of your page objects", function () {});
 });
