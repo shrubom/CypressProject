b54110b5794e05781f9741222b45e4e340411dba
shrubom <shruti.panicker21@gmail.com>
2022-10-01 12:25:08 +0300

    Added new test cases

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "integration/Test3.js",
+    specPattern: "integration/Test4.js",
   },
 });
--- cypress/integration/Test3.js
+++ cypress/integration/Test3.js
@@ -5,7 +5,27 @@ describe("My third test suite", function () {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.get("#alertbtn").click();
     //Now clicking on confirm button
-    cy.get("input[value='Confirm']").click();
+
     //Handling the text inside the pop ups. Cypress listens to core browser events - window alerts.
+    cy.on("window:alert", (str) => {
+      //Mocha string comparison
+      expect(str).to.equal(
+        "Hello , share this practice page and share your knowledge"
+      );
+    });
+    cy.get("input[value='Confirm']").click();
+    cy.on("window:confirm", (confirmStr) => {
+      //Mocha string comparison
+      expect(confirmStr).to.equal("Hello , Are you sure you want to confirm?");
+    });
+  });
+
+  it("Handling child tabs with combination of cypress and jquery commands", function () {
+    cy.visit("https://qaclickacademy.com/practice.php");
+    cy.get("#opentab").invoke("removeAttr", "target").click();
+    cy.wait(2000);
+    cy.go("back");
+    //retrieveing Current URL of the page is active
+    cy.url().should("include", "rahulshetty");
   });
 });
