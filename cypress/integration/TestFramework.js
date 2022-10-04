5b12f7f2d5416c110e0458bc12ecaef79de4255f
shrubom <shruti.panicker21@gmail.com>
2022-10-04 14:50:14 +0300

    Adding Fixtures

--- cypress/integration/TestFramework.js
+++ cypress/integration/TestFramework.js
@@ -1,8 +1,14 @@
 /// <reference types = 'cypress'/>
 
 describe("Cypress Test Framework Test Suite", function () {
-  before("Opening the url", function () {});
+  before("Opening the url", function () {
+    cy.fixture("testData.json").then(function (data) {
+      this.testData = data;
+    });
+  });
   it("Mocha Test Framework", function () {
-    cy.get("input.search-keyword").type("ca");
+    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+    cy.get('input[name="name"]:nth-child(2)').type(this.testData.name);
+    cy.get("select").select(this.testData.gender);
   });
 });
