51509b15b832b454fe4fe1cf6b1819fe067bf746
shrubom <shruti.panicker21@gmail.com>
2022-10-04 13:39:56 +0300

    TestFramework

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "integration/Test4.js",
+    specPattern: "integration/TestFramework.js",
   },
 });
