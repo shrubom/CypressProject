b335ff9b9b802f51b5c81e4296ab5451630d80b1
shrubom <shruti.panicker21@gmail.com>
2022-10-07 15:36:39 +0300

    Page Object Model Implementation

---
+++ cypress/integration/PageObjects/HomePage.js
@@ -0,0 +1,19 @@
+class HomePage {
+  //You add the methods for grasping the dofferent page objects.
+  getEditBox() {
+    return cy.get('input[name="name"]:nth-child(2)');
+  }
+  getTwoWayDataBinding() {
+    return cy.get(":nth-child(4) > .ng-untouched");
+  }
+  getGender() {
+    return cy.get("select");
+  }
+  getEnterprenuer() {
+    return cy.get("#inlineRadio3");
+  }
+  getShopTab() {
+    return cy.get(":nth-child(2) > .nav-link");
+  }
+}
+export default HomePage;
